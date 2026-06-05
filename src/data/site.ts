80190b86d705c5b18f5151e29d3e1bee611c0b0e
cherinojoel-lang <cherinojoel@gmail.com>
2026-06-05 16:36:26 +0200

    refactor(content): remove briefing text and align HSB brand defaults

--- src/data/site.ts
+++ src/data/site.ts
@@ -6,9 +6,9 @@ export const site = {
   phone: "+49 (0)2562 9463030",
   description:
     "Industrieböden, Säureschutz, Keramik, PU-Beton, Entwässerung und Sanierung für Lebensmittel-, Getränke-, Pharma- und Chemieproduktion.",
-  defaultTitle: "Industrieböden & Säureschutz für Produktion | Hexagon Säurebau",
+  defaultTitle: "Industrieböden & Säureschutz für Produktion | HSB Hexagon Säurebau",
   defaultDescription:
-    "Industrieböden, Säureschutz, Keramik, PU-Beton, Entwässerung und Sanierung für Lebensmittel-, Getränke-, Pharma- und Chemieproduktion. Jetzt Belastungsprofil prüfen lassen.",
-  ctaLabel: "Belastungsprofil prüfen lassen",
+    "Industrieböden, Säureschutz, Keramik, PU-Beton, Entwässerung und Sanierung für Lebensmittel-, Getränke-, Pharma- und Chemieproduktion. Jetzt kostenlose Ersteinschätzung anfordern.",
+  ctaLabel: "Ersteinschätzung anfordern",
   ctaTarget: "/kontakt/",
 };
